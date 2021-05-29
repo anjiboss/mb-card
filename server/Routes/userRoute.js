e9bc81dabc185c899a1720ec0a84de54467e7a7a
Anjiboss <nhu.nhu.face@gmail.com>
2021-05-29 15:36:02 +0900

    add get for api

--- server/Routes/userRoute.js
+++ server/Routes/userRoute.js
@@ -2,6 +2,20 @@ const router = require("express").Router();
 // const User = require("../models/User.js");
 const User = require("../models/User.js");
 
+router.get("/:userId", (req, res) => {
+  const userId = req.params.userId;
+  console.log(userId);
+  User.findOne(
+    {
+      userId: userId,
+    },
+    (error, user) => {
+      if (error) return console.error(error);
+      res.json(user);
+    }
+  );
+});
+
 router.post("/", async (req, res) => {
   const { userId, password, info, contact, setting, quotes } = req.body;
   // console.log({ userId, password, info, contact, setting, quotes });
